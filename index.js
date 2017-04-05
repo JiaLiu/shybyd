f822165522469a81440b027f9431571635bfec15
i071817 <jia.liu02@sap.com>
2017-04-06 07:11:27 +0800

    i

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-const request = require('superagent');
+const charset = require('superagent-charset');
+const request = charset(require('superagent'));
 const cheerio = require('cheerio');
+const some = require('lodash.some');
+const strigify = require('csv-stringify');
 
-request.get('http://202.96.245.182/xxcx/ddyd.jsp?lm=6').then(res => {
-    const $ = cheerio.load(res.text);
-    $('#main table:nth-of-type(2) tr').each((i, tr) => console.log(tr));
-});
+const results = [];
+function loadPage(pageNum) {
+    console.log("Request the page %d", pageNum);
+    return request.post('http://202.96.245.182/xxcx/ddyd.jsp').retry(10).type('form').send({ pageno: pageNum }).charset('gbk').then(res => {
+        const $ = cheerio.load(res.text);
+        let count = 0;
+        $('#main table:nth-of-type(2) tr').filter((i) => i % 2 === 1 && i > 1).each((i, tr) => {
+            const children = $(tr).children();
+            const object = {
+                district: $(children[0]).text().trim(),
+                name: $(children[1]).text().trim(),
+                address: $(children[2]).text().trim(),
+                comments: $(children[3]).text().trim()
+            };
+            if (!some(results, object)) {
+                results.push(object);
+                count++;
+            }
+        });
+        console.log("The page %d adds %d items", pageNum, count);
+        return $;
+    });
+}
+
+loadPage(1).then($ => {
+    const children = $('.yypages').children();
+    const pageCount = +($(children[children.length - 2]).text());
+    console.log('Page count: %d', pageCount);
+    return pageCount;
+}).then(pageCount => {
+    const promises = [];
+    for (let i = 2; i <= pageCount; i++) {
+        promises.push(loadPage(i));
+    }
+    return Promise.all(promises);
+}).then(() => strigify(results, (err, output) => {
+    console.log(output);
+}));
